0a777d3bd38dd26f0cc30c96c51ab2fd155511b9
Giorgi Asanidze <gasanidze@tbcbank.com.ge>
2022-06-04 21:33:07 +0400

    first commit

---
+++ DBFirstEF/DBFirstEF/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBFirstEF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WarehouseEntities1 model = new WarehouseEntities1();
+            User us = new User()
+            {
+                Name = textBox1.Text,
+                LastName = textBox2.Text,
+                Nickname = textBox3.Text,
+                Password = textBox4.Text,
+                WarehouseId = 1,
+                RoleId = 1
+            };
+            model.Users.Add(us);
+            model.SaveChanges();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.ShowDialog();
+        }
+    }
+}
---
+++ DBFirstEF/DBFirstEF/Form2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBFirstEF
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WarehouseEntities1 model = new WarehouseEntities1();
+            List<User> users = model.Users.ToList();
+            dataGridView1.DataSource = users;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var user_Id = int.Parse(dataGridView1.CurrentRow.Cells[0].Value.ToString());
+            WarehouseEntities1 model = new WarehouseEntities1();
+            var selectedUser = model.Users.Where(i => i.Id == user_Id).FirstOrDefault();
+            model.Users.Remove(selectedUser);
+            model.SaveChanges();
+        }
+    }
+}
